b67cfd7618ead1d47bc8c829f3ad357b04a63637
Rick Qiu <qiudejun@gmail.com>
2013-11-18 15:05:33 -0800

    more

--- queue/include/queue.hxx
+++ queue/include/queue.hxx
@@ -1,8 +1,13 @@
 #ifndef LINUX_QUEUE_QUEUE_HXX
 #define LINUX_QUEUE_QUEUE_HXX
 
+#include <pthread.h>
+
 #include <cstdint>
 #include <cstring>
+
+#include <queue>
+#include <exception>
 
 #ifdef DEBUG
 #include <cstdio>
@@ -104,6 +109,52 @@ namespace linux {
             std::uint64_t writer_lock;
             constexpr static const std::uint64_t buffer_size = 65536;
             T* circular_buffer[buffer_size];
+        };
+
+        template<typename T>
+        class queue {
+            queue(): lock(PTHREAD_MUTEX_INITIALIZER),
+                     slot_available(PTHREAD_COND_INITIALIZER),
+                     item_available(PTHREAD_COND_INITIALIZER) {
+            }
+            bool empty() const {
+                pthread_mutex_lock(&lock);
+                auto result = container.empty();
+                pthread_mutex_unlock(&lock);
+                return result;
+            }
+            std::size_t size() const {
+                pthread_mutex_lock(&lock);
+                auto result = container.size();
+                pthread_mutex_unlock(&lock);
+                return result;
+            }
+            void push(const T& e) {
+                pthread_mutex_lock(&lock);
+                while(container.size() >= max_size) {
+                    pthread_cond_wait(&slot_available, &lock);
+                }
+                container.push(e);
+                pthread_cond_signal(&item_available);
+                pthread_mutex_unlock(&lock);
+            }
+            T pop() {
+                pthread_mutex_lock(&lock);
+                while(container.empty()) {
+                    pthread_cond_wait(&item_available, &lock);
+                }
+                auto result = container.front();
+                container.pop();
+                pthread_cond_signal(&slot_available);
+                pthread_mutex_unlock(&lock);
+                return result;
+            }
+        private:
+            pthread_mutex lock;
+            pthread_cond_t slot_available;
+            pthread_cond_t item_available;
+            std::queue<T>  container;
+            constexpr static const std::size_t max_size = 1024;
         };
     }
 }
